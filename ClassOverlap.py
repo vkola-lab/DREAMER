b6d2903c30dd9245b74238d83db2e5aedf5ad73e
Meysam Ahangaran <81852309+me-ahangaran@users.noreply.github.com>
2023-02-17 21:17:09 +0330

    Add files via upload

--- ClassOverlap.py
+++ ClassOverlap.py
@@ -18,7 +18,7 @@ class ClassOverlap:
         self
     
     
-    #get number of samples of each five classes of DX_bl
+    #get number of samples of each class. Last column is always the class variable.
     def getNumofSamples(self, data, className):
         rows = len(data)
         cols = len(data[0])
@@ -34,7 +34,7 @@ class ClassOverlap:
         
         rows = len(data)
         cols = len(data[0])
-        data_num = np.array(data[0:(rows-1),0:(cols-1)], dtype=np.float32)
+        data_num = np.array(data[0:(rows),0:(cols-1)], dtype=np.float32)
         data_class = np.array(data[:,(cols-1)], dtype=np.str0)
         
         kNN_ctr = 0 #number of samples in the className with overlapping property
@@ -44,7 +44,7 @@ class ClassOverlap:
                kNN_otherClass = self.getKnnOtherClass(data_num, data_class, className, i)
                if kNN_otherClass > self.tetha:
                    kNN_ctr = kNN_ctr + 1
-        
+        print('num of other class for class ',className,' is: ', kNN_ctr)
         return kNN_ctr
         
                 
@@ -54,66 +54,76 @@ class ClassOverlap:
         kNN_otherClass = 0
         kNN_arrIndexes = _getKnnOtherClass(data_num, idx, self.k)
         for i in range(self.k):
-            # print(data_class[kNN_arrIndexes[i]], className)
+            
             if data_class[kNN_arrIndexes[i]] != className:
                 kNN_otherClass = kNN_otherClass + 1
         
         return kNN_otherClass
     
-    # calculate the R-Value for the input data (last column is DX_bl)
+    
+    
+    # calculate the R-Value for the input data (last column is target)
     def getRvalue(self, data):
         
-        #five classes of DX_bl
-        classCN = 'CN'
-        classLMCI = 'LMCI'
-        classEMCI = 'EMCI'
-        classSMC = 'SMC'
-        classAD = 'AD'
+        classNames_list = getTargetArray(data)
+        numOfClasses = len(classNames_list)
+        totalSamples = 0
+        total_RValue = 0
+        print('Class names = ', classNames_list)
         
-        CN_samples = self.getNumofSamples(data, classCN)
-        LMCI_samples = self.getNumofSamples(data, classLMCI)
-        EMCI_samples = self.getNumofSamples(data, classEMCI)
-        SMC_samples = self.getNumofSamples(data, classSMC)
-        AD_samples = self.getNumofSamples(data, classAD)
+        for i in range(numOfClasses):
+            samples = self.getNumofSamples(data, classNames_list[i])
+            totalSamples = totalSamples + samples
         
-        totalSamples = CN_samples+LMCI_samples+EMCI_samples+SMC_samples+AD_samples
-
+        for j in range(numOfClasses):
+            RVal = self.getRvalueClass(data, classNames_list[j])
+            total_RValue = total_RValue + RVal
         
-        kNN_CN = self.getRvalueClass(data, classCN)
-        kNN_LMCI = self.getRvalueClass(data, classLMCI)
-        kNN_EMCI = self.getRvalueClass(data, classEMCI)
-        kNN_SMC = self.getRvalueClass(data, classSMC)
-        kNN_AD = self.getRvalueClass(data, classAD)
-        
-        R_value = (kNN_CN+kNN_LMCI+kNN_EMCI+kNN_SMC+kNN_AD) / totalSamples
-        # print('R value is ',R_value)
+        R_value = total_RValue / totalSamples
         return R_value
+    
+
+#get array of class names
+def getTargetArray(data):
+        rows = len(data)
+        cols = len(data[0])
+        classNames = [] #list of all class names
+        
+        for i in range(rows):
+            target = data[i, (cols-1)]
+            if target in classNames:
+                continue
+            classNames.append(target)
+        return classNames
+
 
 @numba.jit(nopython=True)
 def _getKnnOtherClass(data, idx, k):
-    
-    rows = len(data)
-    cols = len(data[0])
-    distArr = np.zeros(rows, dtype=np.float32) #distances of samples to the idx sample
-    kNN_arrIndexes = np.zeros(k, dtype=np.int32) #array of kNN indexess
-    
-    for i in range(rows):
-        # distArr[i] = distance.euclidean(data[i, 0:(cols-1)], data[idx, 0:(cols-1)])
-        v1 = data[i, :]
-        v2 = data[idx, :]
-        distArr[i] = np.sqrt(np.sum((v1 - v2) ** 2))
         
-    for i in range(k):
-        if idx!=0:
-            minIdx = 0 #first element is the min element    
-        else:
-            minIdx = 1
-        for j in range(rows):
-            if (j==idx or j in kNN_arrIndexes):  #current index element is discarded
-                continue
-            if distArr[j] <= distArr[minIdx]:
-                minIdx = j
-        kNN_arrIndexes[i] = minIdx                                                           
-    return kNN_arrIndexes
+        rows = len(data)
+        cols = len(data[0])
+        distArr = np.zeros(rows, dtype=np.float32) #distances of samples to the idx sample
+        kNN_arrIndexes = np.zeros(k, dtype=np.int32) #array of kNN indexess
+        
+        for i in range(rows):
+            v1 = data[i, :]
+            v2 = data[idx, :]                
+            distArr[i] = np.sqrt(np.sum((v1 - v2) ** 2))
+            
+        
+        for i in range(k):           
+            for k in range(rows):
+                if ~(k in kNN_arrIndexes):
+                    minIdx = k
+                    break
+              
+            for j in range(rows):
+                if (j==idx or j in kNN_arrIndexes):  #current index element is discarded
+                    continue
+                if distArr[j] <= distArr[minIdx]:
+                    minIdx = j
+            kNN_arrIndexes[i] = minIdx                                                           
+        
+        return kNN_arrIndexes
 
     
